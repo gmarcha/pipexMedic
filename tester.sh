053487b7a705973dab5253d6765217ff11f8bf06
gmarcha <gmarcha@github.com>
2021-06-09 01:18:14 +0200

    update script

--- tester.sh
+++ tester.sh
@@ -30,31 +30,31 @@ printf "\n$S1$RESET\n"
 printf "$BLUE"; make -C ../ all; printf "$RESET"
 
 printf "$YELLOW$S1$RESET\n"
-printf "$PURPLE""test: < test1 grep hello | awk '{count++} END {print count}' > test2$RESET\n"
+printf "$PURPLE""test: < infile grep hello | awk '{count++} END {print count}' > outfile$RESET\n"
 printf "$PURPLE$S2$RESET\n"
-< test1 grep hello | awk '{count++} END {print count}' > test2
-../pipex user1 "grep hello" "awk '{count++} END {print count}'" user2
+< test1 grep hello | awk '{count++} END {print count}' > test2 2> test2
+../pipex user1 "grep hello" "awk '{count++} END {print count}'" user2 2> user2
 comp test2 user2
 rm -rf test2 user2
 
 printf "$YELLOW$S1$RESET\n"
-printf "$PURPLE""test: < test1 grep hello | awk \"{count++} END {print count}\" > test2$RESET\n"
+printf "$PURPLE""test: < infile grep hello | awk \"{count++} END {print count}\" > outfile$RESET\n"
 printf "$PURPLE$S2$RESET\n"
-< test1 grep hello | awk "{count++} END {print count}" > test2
-../pipex user1 "grep hello" "awk \"{count++} END {print count}\"" user2
+< test1 grep hello | awk "{count++} END {print count}" > test2 2> test2
+../pipex user1 "grep hello" "awk \"{count++} END {print count}\"" user2 2> user2
 comp test2 user2
 rm -rf test2 user2
 
 printf "$YELLOW$S1$RESET\n"
-printf "$PURPLE""test: < test1 grep hello | awk '\"{count++} END {print count}\"' > test2$RESET\n"
+printf "$PURPLE""test: < infile grep hello | awk '\"{count++} END {print count}\"' > outfile$RESET\n"
 printf "$PURPLE$S2$RESET\n"
-< test1 grep hello | awk '"{count++} END {print count}"' > test2
-../pipex user1 "grep hello" "awk '\"{count++} END {print count}\"'" user2
+< test1 grep hello | awk '"{count++} END {print count}"' > test2 2> test2
+../pipex user1 "grep hello" "awk '\"{count++} END {print count}\"'" user2 2> user2
 comp test2 user2
 rm -rf test2 user2
 
 printf "$YELLOW$S1$RESET\n"
-printf "$PURPLE""test: < test1 grep hello | awk \"'{count++} END {print count}'\" > test2$RESET\n"
+printf "$PURPLE""test: < infile grep hello | awk \"'{count++} END {print count}'\" > outfile$RESET\n"
 printf "$PURPLE$S2$RESET\n"
 < test1 grep hello | awk "'{count++} END {print count}'" > test2 2> test2
 ../pipex user1 "grep hello" "awk \"'{count++} END {print count}'\"" user2 2> user2
@@ -62,3 +62,5 @@ comp test2 user2
 rm -rf test2 user2
 
 rm -rf test1 user1
+
+printf "\033[38;2;255;176;0m\n%80s\n%80s\n%80s\033[0m\n" "Test finished." "@2021, Pipex Medic." "https://github.com/gmarcha"
