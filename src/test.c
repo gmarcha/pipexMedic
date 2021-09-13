a85f5b9d85495fcb4dd5d7b2f3b8e13741abb89f
gmarcha <gmarcha@github.com>
2021-09-13 19:29:07 +0200

    update src: add output and error file with redirection, and pipex execution

--- src/test.c
+++ src/test.c
@@ -4,6 +4,10 @@
 
 void test(char *inputFileContent, char *commandList[], char *envp[]) {
 
+    int     fd_output;
+    int     fd_error;
+    FILE    *fs_return;
+
     pid_t pid = fork();
 
     if (pid == (pid_t)-1) {
@@ -12,7 +16,18 @@ void test(char *inputFileContent, char *commandList[], char *envp[]) {
 
     else if (pid == (pid_t)0) {
 
+        char **pipexArgs = createPipexArgs(commandList);
 
+        if (
+            (fd_output = open("./tmp/output.tmp", O_WRONLY | O_CREAT | O_TRUNC, 0644)) == -1 ||
+            (fd_error = open("./tmp/error.tmp", O_WRONLY | O_CREAT | O_TRUNC, 0644)) == -1
+        ) {
+            ferror("open", errno);
+        }
+
+        if (dup2(fd_output, STDOUT_FILENO) == -1 || dup2(fd_error, STDERR_FILENO) == -1) {
+            ferror("dup2", errno);
+        }
 
         int execStatus = execve("./pipex", pipexArgs, envp);
         if (execStatus == -1) {
@@ -22,5 +37,8 @@ void test(char *inputFileContent, char *commandList[], char *envp[]) {
 
     else {
 
+        if ((fs_return = fopen("./tmp/return.tmp", "w")) == NULL) {
+
+        }
     }
 }
