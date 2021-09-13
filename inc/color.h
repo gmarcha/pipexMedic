730f79fa227d9b0aa102529123408172aa686623
gmarcha <gmarcha@github.com>
2021-09-13 13:06:35 +0200

    update header: add color

---
+++ inc/color.h
@@ -0,0 +1,42 @@
+#ifndef COLOR_H
+# define COLOR_H
+
+# define RESET			"\033[0m"
+
+# define FG_BLACK		"\033[30m"
+# define FG_RED			"\033[31m"
+# define FG_GREEN		"\033[32m"
+# define FG_YELLOW		"\033[33m"
+# define FG_BLUE		"\033[34m"
+# define FG_MAGENTA		"\033[35m"
+# define FG_CYAN		"\033[36m"
+# define FG_LGRAY		"\033[37m"
+
+# define FG_DGRAY		"\033[90m"
+# define FG_LRED		"\033[91m"
+# define FG_LGREEN		"\033[92m"
+# define FG_LYELLOW		"\033[93m"
+# define FG_LBLUE		"\033[94m"
+# define FG_LMAGENTA	"\033[95m"
+# define FG_LCYAN		"\033[96m"
+# define FG_WHITE		"\033[97m"
+
+# define BG_BLACK		"\033[40m"
+# define BG_RED			"\033[41m"
+# define BG_GREEN		"\033[42m"
+# define BG_YELLOW		"\033[43m"
+# define BG_BLUE		"\033[44m"
+# define BG_MAGENTA		"\033[45m"
+# define BG_CYAN		"\033[46m"
+# define BG_LGRAY		"\033[47m"
+
+# define BG_DGRAY		"\033[100m"
+# define BG_LRED		"\033[101m"
+# define BG_LGREEN		"\033[102m"
+# define BG_LYELLOW		"\033[103m"
+# define BG_LBLUE		"\033[104m"
+# define BG_LMAGENTA	"\033[105m"
+# define BG_LCYAN		"\033[106m"
+# define BG_WHITE		"\033[107m"
+
+#endif // COLOR_H
