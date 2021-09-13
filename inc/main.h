0a0efd99e6a5d814fdc008f9db703e9e40de41c1
gmarcha <gmarcha@github.com>
2021-09-13 18:05:57 +0200

    update header: add main, and test

---
+++ inc/main.h
@@ -0,0 +1,13 @@
+#ifndef MAIN_H
+# define MAIN_H
+
+# include "test.h"
+# include "color.h"
+
+// 1. Mandatory
+// 2. Bonus
+
+// a. Output
+// b. Return value
+
+#endif // MAIN_H
---
+++ inc/test.h
@@ -0,0 +1,16 @@
+#ifndef TEST_H
+# define TEST_H
+
+# include <stdio.h>
+# include <stdlib.h>
+# include <unistd.h>
+# include <fcntl.h>
+# include <errno.h>
+# include <string.h>
+# include <signal.h>
+# include <sys/wait.h>
+
+# define PROGRAM_NAME                   "pipexMedic"
+# define ferror(funName, errNum)        fprintf(stderr, "%s: %s failed: %s\n", PROGRAM_NAME, funName, strerror(errNum)); exit(1);
+
+#endif // TEST_H
