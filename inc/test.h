78349f0f5d5ca2a7a96f23bc50629d90072cf7de
gmarcha <gmarcha@github.com>
2021-09-14 13:38:46 +0200

    update header: add defines to path and env

--- inc/test.h
+++ inc/test.h
@@ -12,6 +12,11 @@
 # include <sys/wait.h>
 
 # define PROGRAM_NAME                   "pipexMedic"
+# define DEFAULT_PATH                   "PATH=/usr/local/bin:/usr/bin:/bin:/usr/local/sbin:/usr/sbin:/sbin"
+# define DEFAULT_ENV                    (char *[]){ DEFAULT_PATH, NULL }
+# define NULL_ENV                       (char *[]){ NULL }
+
+# define ARGS(...)                      (char *[]){ __VA_ARGS__, NULL }
 # define ferror(funName, errNum)        fprintf(stderr, "%s: %s failed: %s\n", PROGRAM_NAME, funName, strerror(errNum)); exit(1);
 
 #endif // TEST_H
