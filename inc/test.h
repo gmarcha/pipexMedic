caf45dfeb411bb5ed1d800575d52b0392a4f0c37
gmarcha <gmarcha@github.com>
2021-09-17 01:00:43 +0200

    update header: update test header, update macros, includes and prototypes of tests

--- inc/test.h
+++ inc/test.h
@@ -8,17 +8,31 @@
 # include <fcntl.h>
 # include <errno.h>
 # include <string.h>
+# include <time.h>
+# include <pthread.h>
 # include <signal.h>
+# include <stdint.h>
 # include <sys/wait.h>
+# include <sys/stat.h>
+# include <sys/types.h>
 
 # define PROGRAM_NAME                   "pipexMedic"
+# define TIMEOUT                        10
 # define DEFAULT_PATH                   "PATH=/usr/local/bin:/usr/bin:/bin:/usr/local/sbin:/usr/sbin:/sbin"
-# define DEFAULT_ENV                    (char *[]){ DEFAULT_PATH, NULL }
-# define NULL_ENV                       (char *[]){ NULL }
+# define DEFAULT_ENV                    (char *const []){ DEFAULT_PATH, NULL }
+# define NULL_ENV                       (char *const []){ NULL }
 
-# define ARGS(...)                      (char *[]){ __VA_ARGS__, NULL }
+# define ARGS(...)                      (char *const []){ __VA_ARGS__, NULL }
 # define ferror(funName, errNum)        fprintf(stderr, "%s: %s failed: %s\n", PROGRAM_NAME, funName, strerror(errNum)); exit(1);
 
-void test(char *inputFileContent, char *commandList[], char *envp[]);
+typedef struct s_test {
+
+    char *const*    commandList;
+    char *const*    envp;
+    const char*     inputFileContent;
+}                   t_test;
+
+void    test(const char *testSubset, const size_t testIndex, char *const commandList[], char *const envp[], const char* inputFileContent);
+void    runTest(const char *testSubset, const size_t testIndex, char *const commandList[], char *const envp[], const char* inputFileContent);
 
 #endif // TEST_H
