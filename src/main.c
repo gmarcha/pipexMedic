878e355c819cc227c0c2f1c3a20df256d2c3e3d4
gmarcha <gmarcha@github.com>
2021-10-25 20:31:55 -0400

    update src: add multiple command tests

--- src/main.c
+++ src/main.c
@@ -41,6 +41,12 @@ int main(int ac, char *av[]) {
         { ARGS("sleep 3", "sleep 3"), DEFAULT_ENV, "Hello World!\n" }
     };
 
+    const t_test multiple_commandTests[] = {
+        { ARGS("ls -la src/", "grep .c", "wc -l"), DEFAULT_ENV, "Hello World!\n" },
+        { ARGS("tr -d !", "grep -v !", "sed 's/Hello/Salut/g'"), DEFAULT_ENV, "Hello World!\nHello World!\nHello World!\nHello World!\nHello World!\n" },
+        { ARGS("tr -d !", "grep -v !", "sed 's/Hello/Salut/g'", "grep Salut", "wc -l"), DEFAULT_ENV, "Hello World!\nHello World!\nHello World!\nHello World!\nHello World!\n" }
+    };
+
     if (
         strcmp(inputSubsetTest, "basic") == 0 ||
         strcmp(inputSubsetTest, "mandatory") == 0 ||
@@ -85,6 +91,22 @@ int main(int ac, char *av[]) {
         for (size_t i = 0; i < 1; i++) {
             if (inputSpecificTest == 0 || inputSpecificTest == i + 1) {
                 runTest("CONCURRENCY", i + 1, concurrencyTests[i].commandList, concurrencyTests[i].envp, concurrencyTests[i].inputFileContent);
+            }
+        }
+    }
+
+    if (
+        strcmp(inputSubsetTest, "multiple_command") == 0 ||
+        strcmp(inputSubsetTest, "bonus") == 0 ||
+        strcmp(inputSubsetTest, "b") == 0 ||
+        strcmp(inputSubsetTest, "all") == 0 ||
+        strcmp(inputSubsetTest, "a") == 0
+    ) {
+
+        printf(FG_YELLOW"\nmultiple command tests\n"RESET);
+        for (size_t i = 0; i < 3; i++) {
+            if (inputSpecificTest == 0 || inputSpecificTest == i + 1) {
+                runTest("MULTIPLE COMMAND", i + 1, multiple_commandTests[i].commandList, multiple_commandTests[i].envp, multiple_commandTests[i].inputFileContent);
             }
         }
     }
