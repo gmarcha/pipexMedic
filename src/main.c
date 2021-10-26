1dce460b24c6783bae7eab16fb4a9875e5589c84
gmarcha <gmarcha@github.com>
2021-10-25 20:15:23 -0400

    update src: update error and concurrency tests

--- src/main.c
+++ src/main.c
@@ -25,26 +25,66 @@ int main(int ac, char *av[]) {
         { ARGS("grep Hello", "awk '{count++} END {print count}'"), DEFAULT_ENV, "Hello World!\nHello World!\n" },
         { ARGS("grep Hello", "awk \"{count++} END {print count}\""), DEFAULT_ENV, "Hello World!\nHello World!\n" },
         { ARGS("grep Hello", "awk '\"{count++} END {print count}\"'"), DEFAULT_ENV, "Hello World!\nHello World!\n" },
-        { ARGS("grep Hello", "awk \"'{count++} END {print count}'\""), DEFAULT_ENV, "Hello World!\nHello World!\n" },
-        { ARGS("fizzBuzz", "ls -la src/"), DEFAULT_ENV, "Hello World!\n" },
-        { ARGS("ls -la src/", "buzzFizz"), DEFAULT_ENV, "Hello World!\n" },
+        { ARGS("grep Hello", "awk \"'{count++} END {print count}'\""), DEFAULT_ENV, "Hello World!\nHello World!\n" }
+    };
+
+    const t_test errorTests[] = {
         { ARGS("grep Hello", "wc -l"), DEFAULT_ENV, NULL },
         { ARGS("grep Hello", "wc -l"), NULL_ENV, "Hello World!\n" },
+        { ARGS("fizzBuzz", "ls -la src/"), DEFAULT_ENV, "Hello World!\n" },
+        { ARGS("ls -la src/", "buzzFizz"), DEFAULT_ENV, "Hello World!\n" },
         { ARGS("fizzBuzz", "wc -l"), NULL_ENV, "Hello World!\n" },
-        { ARGS("grep Hello", "buzzFizz"), NULL_ENV, "Hello World!\n" },
+        { ARGS("grep Hello", "buzzFizz"), NULL_ENV, "Hello World!\n" }
+    };
+
+    const t_test concurrencyTests[] = {
         { ARGS("sleep 3", "sleep 3"), DEFAULT_ENV, "Hello World!\n" }
     };
 
     if (
         strcmp(inputSubsetTest, "basic") == 0 ||
         strcmp(inputSubsetTest, "mandatory") == 0 ||
-        strcmp(inputSubsetTest, "m") == 0
+        strcmp(inputSubsetTest, "m") == 0 ||
+        strcmp(inputSubsetTest, "all") == 0 ||
+        strcmp(inputSubsetTest, "a") == 0
     ) {
 
-        printf(FG_YELLOW"basic tests: basic commands, error handling and concurrency\n"RESET);
-        for (size_t i = 0; i < 15; i++) {
+        printf(FG_YELLOW"basic tests\n"RESET);
+        for (size_t i = 0; i < 8; i++) {
             if (inputSpecificTest == 0 || inputSpecificTest == i + 1) {
                 runTest("BASIC", i + 1, basicTests[i].commandList, basicTests[i].envp, basicTests[i].inputFileContent);
+            }
+        }
+    }
+
+    if (
+        strcmp(inputSubsetTest, "error") == 0 ||
+        strcmp(inputSubsetTest, "mandatory") == 0 ||
+        strcmp(inputSubsetTest, "m") == 0 ||
+        strcmp(inputSubsetTest, "all") == 0 ||
+        strcmp(inputSubsetTest, "a") == 0
+    ) {
+
+        printf(FG_YELLOW"\nerror handling\n"RESET);
+        for (size_t i = 0; i < 6; i++) {
+            if (inputSpecificTest == 0 || inputSpecificTest == i + 1) {
+                runTest("ERROR", i + 1, errorTests[i].commandList, errorTests[i].envp, errorTests[i].inputFileContent);
+            }
+        }
+    }
+
+    if (
+        strcmp(inputSubsetTest, "concurrency") == 0 ||
+        strcmp(inputSubsetTest, "mandatory") == 0 ||
+        strcmp(inputSubsetTest, "m") == 0 ||
+        strcmp(inputSubsetTest, "all") == 0 ||
+        strcmp(inputSubsetTest, "a") == 0
+    ) {
+
+        printf(FG_YELLOW"\nconcurrency launching for pipeline\n"RESET);
+        for (size_t i = 0; i < 1; i++) {
+            if (inputSpecificTest == 0 || inputSpecificTest == i + 1) {
+                runTest("CONCURRENCY", i + 1, concurrencyTests[i].commandList, concurrencyTests[i].envp, concurrencyTests[i].inputFileContent);
             }
         }
     }
